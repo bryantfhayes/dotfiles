2c339c40c6e5461852cb4b0a8c360995b0fe5eb7
Bryant Hayes <bryant.hayes@cognex.com>
2019-10-29 04:52:30 +0000

    fix install

--- install.sh
+++ install.sh
@@ -2,9 +2,16 @@
 
 DOTFILES_ROOT=~/dotfiles
 
+# Install zsh
+sudo apt-get install zsh
+
+# Install Oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 ln -s $DOTFILES_ROOT/zshrc ~/.zshrc
 ln -s $DOTFILES_ROOT/vimrc ~/.vimrc
-ln -s ~/dotfiles/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
+ln -s $DOTFILES_ROOT/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
 
